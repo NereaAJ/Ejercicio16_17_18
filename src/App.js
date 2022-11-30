fd697359a54b21256b8c8330ac00fd220be2afa2
NereaAJ <nalvarez@serbatic.es>
2022-11-30 14:46:17 +0100

    actualizacion  task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
+import { useEffect } from 'react';
+
 import './App.css';
-import TaskListComponents from './components/container/task_list';
-import Loginformik from './components/pure/form/loginFormik';
-import Registerformik from './components/pure/form/registerFormik';
+
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+
+import HomePage from './pages/home/HomePage';
+import Statepage from './pages/home/StatePage';
+
+import Notfoundpage from './pages/404/NotFountPage';
+import Taskspage from './pages/tasks/TasksPage';
+import Loginpage from './pages/auth/LoginPage';
+import LogoutPage from './pages/auth/LogoutPage';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Componente de listado de Estado de Contacto */}
-       {<TaskListComponents></TaskListComponents>}
 
-       {/* <Loginformik></Loginformik> */}
-       {/* <Registerformik></Registerformik> */}
-      </header>
-    </div>
+  let logged = false;
+
+  useEffect(() => {
+    logged = localStorage.getItem('credentials');
+    console.log('User Logged?', logged)
+  }, [])
+
+
+  return (
+    <Router>
+      <div>
+        <aside>
+          <Link to='/'>|| HOME |</Link>
+          
+          <Link to='/login'>| Login |</Link>
+          <Link to='/registro'>| Registro |</Link>
+
+          <Link to='/task/1'>| Task ||</Link>
+        </aside>
+
+        <main>
+          <Switch> 
+            <Route exact path='/' component={ HomePage } />
+            <Route exact path='/online-state' component={ Statepage } />
+            <Route path='/login' component= { Loginpage }>
+              {
+                  logged ? 
+                    () => {
+                      alert('You are logged in. Redirecting to home...')
+                      return (<Redirect to='/'/>)
+                    }
+                  :
+                  () => {
+                    return (<Loginpage></Loginpage>)
+                  }
+              }
+            </Route>
+            <Route path='/registro' component={ LogoutPage } />
+            <Route path='/tasks' component={ Taskspage } />
+            {/* 404 - Page No Found */}
+            <Route component={ Notfoundpage } />
+          </Switch>
+        </main>
+
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/pages/auth/LogoutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Registerformik from '../../components/pure/form/registerFormik';
+
+const LogoutPage = () => {
+    return (
+        <div>
+            <Registerformik></Registerformik>
+        </div>
+    );
+}
+
+export default LogoutPage;
